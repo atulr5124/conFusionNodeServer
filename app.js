8fe245f7623f593d84a3a73f1e72d4a3d1e64b5c
Atul Rauthan <atulrauthan7@gmail.com>
2020-06-20 12:58:58 +0530

    Added session functionality

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var session = require('express-session');
+var FileStore = require('session-file-store')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,12 +31,19 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('kakarot-12345-67890-09876-54321'));
+// app.use(cookieParser('kakarot-12345-67890-09876-54321'));
+app.use(session({
+  name: 'session-id',
+  secret: 'kakarot-12345-67890-09876-54321',
+  saveUninitialized: false,
+  resave: false,
+  store: new FileStore()
+}));
 
 function auth(req, res, next) {
-  console.log(req.signedCookies);
+  console.log(req.session);
 
-  if (!req.signedCookies.user) {
+  if (!req.session.user) {
     var authHeader = req.headers.authorization;
     if (!authHeader) {
       var err = new Error('You are not authenticated!');
@@ -48,7 +57,8 @@ function auth(req, res, next) {
     var user = auth[0];
     var pass = auth[1];
     if (user == 'admin' && pass == 'password') {
-      res.cookie('user', 'admin', { signed: true });
+      // res.cookie('user', 'admin', { signed: true });
+      req.session.user = 'admin';
       next(); // authorized
     } else {
       var err = new Error('Invalid username/password!');
@@ -57,7 +67,7 @@ function auth(req, res, next) {
       next(err);
     }
   } else {
-    if(req.signedCookies.user === 'admin') {
+    if(req.session.user === 'admin') {
       next();
     } else {
       var err = new Error('Session expired!');
